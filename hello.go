a4f4f4382b55dee31bc94106dc2059a4a0f41428
billychou <zsc1528@gmail.com>
2019-03-13 23:14:32 +0800

    utils

--- hello.go
+++ hello.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/billychou/go-startup/stringutil"
+	"math/rand"
+)
 
 func main() {
 	fmt.Printf("Hello,world.\n")
+	fmt.Printf(stringutil.Reverse("kskskafjkajaf"))
+	fmt.Println("my favorite number is", rand.Intn(10))
 }
