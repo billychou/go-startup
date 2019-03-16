b79146b71a49526c8d9275ae07681685b09ece50
周松川 <qppzsc@192.168.1.103>
2019-03-16 09:20:11 +0800

    hello

--- hello.go
+++ hello.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"fmt"
-	"github.com/billychou/go-startup/stringutil"
 	"math/rand"
+
+	"github.com/billychou/go-startup/stringutil"
 )
+
+const boilingF = 212.0
 
 func main() {
 	fmt.Printf("Hello,world.\n")
 	fmt.Printf(stringutil.Reverse("kskskafjkajaf"))
+	fmt.Println("Helloworld!")
 	fmt.Println("my favorite number is", rand.Intn(10))
+	fmt.Println("换行")
 }
